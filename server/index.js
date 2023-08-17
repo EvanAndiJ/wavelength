aa77db2862769b0fc79fb53ac37edd6d17644480
Evan Jones <evanandyjones@gmail.com>
2023-08-17 01:21:07 -0700

    secret updated

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const express = require('express');
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express();
-
+//
 const corsOptions = {
   origin: ["http://localhost:3001"]
 };
